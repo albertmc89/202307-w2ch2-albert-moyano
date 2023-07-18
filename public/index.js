c8367ed333e16cfc5e6bed1c1e095b5125e17e7a
Albert Colubi <albertmc89@gmail.com>
2023-07-18 23:52:13 +0200

    Create if statement with logical && in every case with values 0,-0

--- public/index.js
+++ public/index.js
@@ -1,4 +1,12 @@
 const strictEquals = (valueA, valueB) => {
+  if (Object.is(valueA, 0) && Object.is(-0, valueB)) {
+    return true;
+  }
+
+  if (Object.is(valueA, -0) && Object.is(0, valueB)) {
+    return true;
+  }
+
   return Object.is(valueA, valueB);
 };
 
