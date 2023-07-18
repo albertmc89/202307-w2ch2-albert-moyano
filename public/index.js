17e38986911f8cc4ad4faca4e3cc9bac5fc1f449
Albert Colubi <albertmc89@gmail.com>
2023-07-18 23:25:50 +0200

    Build the test cases logging the function with every argument

--- public/index.js
+++ public/index.js
@@ -1,1 +1,10 @@
 const strictEquals = (valueA, valueB) => {};
+
+console.log(strictEquals(1, 1));
+console.log(strictEquals(NaN, NaN));
+console.log(strictEquals(0, -0));
+console.log(strictEquals(-0, 0));
+console.log(strictEquals(1, "1"));
+console.log(strictEquals(true, false));
+console.log(strictEquals(false, false));
+console.log(strictEquals("water", "oil"));
